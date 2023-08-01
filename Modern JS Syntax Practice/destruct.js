0c32f47ecd9846d3309e3b5e6f8a00b6c74473b0
fermions75 <omifarhan@gmail.com>
2023-08-01 16:26:55 +0600

    practice

--- Modern JS Syntax Practice/destruct.js
+++ Modern JS Syntax Practice/destruct.js
@@ -22,11 +22,13 @@ const {education: {degree: z} = {}} = user
 console.log(z);
 
 
+
 // array destructuring
 
 var numbers = [1,2,3,4,5];
 var [,a,,,b] = numbers;
 console.log(a, b);
+
 
 var arr = [1,2,[3,4,100],5];
 var [,,[a,b]] = arr;
